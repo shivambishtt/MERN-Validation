c72f47f05d7f0cf1a90c18cf617cc1226865d100
Shivam Bisht <rajshivam2745@gmail.com>
2024-12-23 12:05:24 +0530

    middleware add

---
+++ backend/controllers/user.controllers.js
@@ -0,0 +1,9 @@
+import { User } from "../models/user.models.js";
+import asyncHandler from "../utils/asyncHandler.js";
+import apiError from "../utils/apiError.js";
+import apiResponse from "../utils/apiResponse.js";
+
+
+const registerUser = asyncHandler(async(req,res)=>{
+
+});
---
+++ backend/middlewares/auth.middleware.js
@@ -0,0 +1,20 @@
+import apiError from "../utils/apiError.JS";
+import asyncHandler from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken";
+
+
+export const verifyJWT = asyncHandler(
+  async((req, _, next) => {
+    try {
+      const token =
+        req?.cookies?.accessToken ||
+        req.header("Authorization")?.replace("Bearer", "");
+
+      if (!token) {
+        throw new apiError(401, "No access token found ");
+      }
+// hume dekhna hai ki hum token ko decode kese karenge 
+      const decodedToken =  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    } catch (error) {}
+  })
+);
