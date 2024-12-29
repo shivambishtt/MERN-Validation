dfda206a47b71d692d3efa6b035801ca34f64e42
Shivam Bisht <rajshivam2745@gmail.com>
2024-12-29 10:45:15 +0530

    client side route add

---
+++ frontend/src/main.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom"
+import Home from './pages/Home.jsx'
+import Login from './pages/Login.jsx'
+import Signup from './pages/Signup.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to={"/login"} />
+  },
+  {
+    path: "/home",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+
+])
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Signup() {
+  return (
+    <div className='container'>
+      <h1>Signup</h1>
+      
+    </div>
+  )
+}
+
+export default Signup
