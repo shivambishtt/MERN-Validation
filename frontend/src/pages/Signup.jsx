02728da7b74ccdb635c387cb7f3ca9d88633e828
Shivam Bisht <rajshivam2745@gmail.com>
2024-12-30 12:23:02 +0530

    frontend and components add

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
+import { useForm } from "react-hook-form"
+import { Input } from '@/components/ui/input'
 
 function Signup() {
-  return (
-    <div className='container'>
-      <h1>Signup</h1>
-      
-    </div>
-  )
+    const { register, control, handleSubmit } = useForm()
+    return (
+        <div className='container flex items-center justify-center'>
+            <div className='bg-gray-400 w-2/4 '>
+            <h1 className='text-3xl font-semibold'>SIGNUP</h1>
+            <form>
+            <Input
+                type="text"
+                placeholder="Enter your Name here"
+            />
+            <Input
+                type="text"
+                placeholder="Enter your Email here"
+            />
+         
+            </form>
+            </div>
+            
+        </div>
+    )
 }
 
 export default Signup
