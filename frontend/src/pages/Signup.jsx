b3a0de44ad2db0659dfeff5d49afc63336302666
Shivam Bisht <rajshivam2745@gmail.com>
2024-12-31 11:51:33 +0530

    validation and frontend add

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,26 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useForm } from "react-hook-form"
 import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
 
 function Signup() {
-    const { register, control, handleSubmit } = useForm()
+    const { register, handleSubmit } = useForm()
+    const [data, setData] = useState()
+
+    const handleChange = (event) => {
+        const { name, value } = event.target
+        setData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }))
+    }
+
+    const handleSignup = (formdata) => {
+        const { name, email } = formdata
+        if (!name || !email) {
+            console.log("Both fields are required");
+        }
+        
+    }
     return (
-        <div className='container flex items-center justify-center'>
-            <div className='bg-gray-400 w-2/4 '>
-            <h1 className='text-3xl font-semibold'>SIGNUP</h1>
-            <form>
-            <Input
-                type="text"
-                placeholder="Enter your Name here"
-            />
-            <Input
-                type="text"
-                placeholder="Enter your Email here"
-            />
-         
-            </form>
+        <div className='py-5 container flex items-center justify-center'>
+            <div className=' box- w-2/4 '>
+                <h1 className='text-3xl font-semibold'>SIGNUP</h1>
+                <form onSubmit={handleSubmit(handleSignup)} >
+                    <Input
+                        type="text"
+                        placeholder="Name here"
+                        {...register("name")}
+                        onChange={handleChange}
+                    /> <br />
+                    <Input
+                        type="text"
+                        placeholder="Email here"
+                        {...register("email")}
+                        onChange={handleChange}
+
+                    />
+                    <Button type="submit">Submit</Button>
+                </form>
+                <br />
             </div>
-            
+
         </div>
     )
 }
